2fabdb7765e4cb131eec26681575e07aff3130ed
TrucNhu135 <tnhutnhu130505@gmail.com>
2026-03-04 18:44:21 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyPhongTroWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLuuChiSo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPhong_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnKhach_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnHopDong_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnChiSoDN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnHoaDon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTKe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void dgKhachThue_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
